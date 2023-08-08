e32dac6da7d6bef511e0b71acb6855516a65a4e3
“mavine4512” <mavineoyugi@gmail.com>
2023-08-08 02:46:43 -0700

    fixed chmod

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
+for x in range(26):
+    print('{:c}'.format(ord('a') + x), end='')
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
+for x in range(ord('a'), ord('z') + 1):
+    if X != ord('q') and x != ord('e'):
+        print('{:c}'.formmat(i), end='')
