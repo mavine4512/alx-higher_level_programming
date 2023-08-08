e844061e2fbc41b83dda73c706393f48f5f9bad3
“mavine4512” <mavineoyugi@gmail.com>
2023-08-08 03:02:18 -0700

    fixed error

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for x in range(ord('a'), ord('z') + 1):
     if X != ord('q') and x != ord('e'):
-        print('{:c}'.formmat(i), end='')
+        print('{:c}'.format(i), end='')
