2e40c8493155c6c3286af8c717c447389a8b0ad4
“mavine4512” <mavineoyugi@gmail.com>
2023-08-14 01:14:54 -0700

    fixed typo

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -20,6 +20,6 @@ void print_python_list_info(PyObject *p)
 		printf("Element %d: ", i);
 
 		obj = PyList_GetItem(p, i);
-		printf("%s\n", PyTYPE(obj)->tp_name);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
 	}
 }
