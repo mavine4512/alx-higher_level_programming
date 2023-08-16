10eeb72906ecf45aef362227644247579a1b5191
“mavine4512” <mavineoyugi@gmail.com>
2023-08-16 06:08:23 -0700

    fixed error

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    multiplied_dict = {}
-
-    for key, value in a_dictionary.items():
-        multiplied_dict[key] = value * 2
-
-    return(multiplied_dict)
+    if a_dictionary is None:
+        return None
+    new_dict = dict()
+    for key in a_dictionary.keys():
+            new_dict[key] = a_dictionary[key] * 2
+    return new_dict
