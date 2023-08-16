32be7e7e27208e00b3b32435eb497030d2d75d6e
“mavine4512” <mavineoyugi@gmail.com>
2023-08-16 05:44:09 -0700

    fixing error

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    return(set_1 & set_2)
+    new_set_list = []
+    for item in set_1:
+        if item in set_2:
+            new_set_list.append(item)
+    return set(new_set_list)
