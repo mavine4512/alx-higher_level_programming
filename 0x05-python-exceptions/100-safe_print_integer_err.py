98d9b818ceefcd0e61f02e03870f8a55af4b15a8
“mavine4512” <mavineoyugi@gmail.com>
2023-08-28 06:22:54 -0700

    Python - Exceptions

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    '''
+    Prints an integer with "{:d}".format().
+    Paraments:
+    value (any): The item to print
+    Returns:
+    True if the item was successfully printed, otherwise False
+    '''
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    '''
+    Prent integer of a list
+    Parameters:
+    my_list(list): The list containing the elemets to print
+    x (int): The number of elements to print
+    Returns:
+    The actual number of elements printed
+    '''
+    num = 0
+    for i in range(0, x):
+        item = my_list[i]
+        try:
+            print("{:d}".format(item), end="")
+            num += 1
+        except (ValueError, TypeError):
+            ontinue
+    print("")
+    return (num)
+
