ed72115d982878d0f8d0e4fa47852c71f3574f74
“mavine4512” <mavineoyugi@gmail.com>
2023-08-30 05:29:56 -0700

    fixed error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,13 +9,12 @@ def safe_print_list(my_list=[], x=0):
     Returns
     Number of elemets
     '''
-    try:
-        k = 0
-        for i in range(x):
+    k = 0
+    for i in range(x):
+        try:
             print("{}".format(my_list=[i]), end="")
             k += 1
-    except IndexError:
-        pass
-    finally:
-        print("")
-        return k
+        except IndexError:
+            break
+    print("")
+    return (k)
