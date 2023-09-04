b7bb0a889efffc0d36d1aca31eb387ddc1acec1f
“mavine4512” <mavineoyugi@gmail.com>
2023-09-04 16:26:02 -0700

    fixed error

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -78,16 +78,16 @@ class Rectangle:
             if column < self.__height - 1:
                 rectangle += "\n"
         return (rectangle)
-    
+
     def __repr__(self):
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
-    
+
     def __del__(self):
         '''prints a message for every object that is deleted.
         '''
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
- 
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
@@ -98,7 +98,7 @@ class Rectangle:
             return rect_1
         else:
             return rect_2
-    
+
     @classmethod
     def square(cls, size=0):
         return Rectangle(size, size)
