e191a9e52fa9e5a8b95a295c65b7bbace8097765
“mavine4512” <mavineoyugi@gmail.com>
2023-09-12 09:05:25 -0700

    fixed issue

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,5 +6,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    '''Saves the JSON representation of an object to a file
+    '''
     with open(filename, "w") as f:
         json.dump(my_obj, f)
