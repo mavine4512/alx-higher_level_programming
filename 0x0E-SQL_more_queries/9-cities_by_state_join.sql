2d30bbb58cd6a8ed02b49ec1b79aab1329c13193
“mavine4512” <mavineoyugi@gmail.com>
2023-10-20 08:12:32 -0700

    fixed 09

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- list of cities
-SELECT cities.id, cities.name, state.name
+SELECT cities.id, cities.name, states.name
 FROM cities
 INNER JOIN states ON cities.state_id=states.id
 ORDER BY cities.id;
