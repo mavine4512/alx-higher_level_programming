895f6bc127c914b0111540b1639245d8ae8d7b21
“mavine4512” <mavineoyugi@gmail.com>
2023-10-20 07:58:18 -0700

    fixed 103

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -10,7 +10,7 @@ WHERE tv_shows.id NOT IN
 (SELECT tv_shows.id
 FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.shows_id
+ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = "Comedy")
