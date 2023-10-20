34ecca7cfab83bd05368b84cb60bd7eeb11d905f
“mavine4512” <mavineoyugi@gmail.com>
2023-10-20 08:19:28 -0700

    fixed 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -7,7 +7,7 @@
 
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-INNER JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
