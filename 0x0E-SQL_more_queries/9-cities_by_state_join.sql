65ba551a75b5f50312fe8195c60570ffd7ad4feb
“mavine4512” <mavineoyugi@gmail.com>
2023-10-20 07:05:39 -0700

    fixe 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
--- lists all show contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT tv_show_genres.genre_id
-FROM tv_shows
-INNER JOIN tv_show_genres
-ON tv_shows.id=tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+-- list of cities
+SELECT cities.id, cities.name, state.name
+FROM cities
+INNER JOIN state ON cities.state_id=states.id
+ORDER BY cities.id
