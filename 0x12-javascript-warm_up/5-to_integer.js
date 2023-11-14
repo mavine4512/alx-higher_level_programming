bfe899db5f5be63af2568c709fbb3c5f7ab3a8ed
“mavine4512” <mavineoyugi@gmail.com>
2023-11-14 02:31:10 -0800

    fixed errors

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,14 +3,14 @@
 // Getting the first argument from the command line
 const arg = process.argv[2];
 
- //convert the argument to integer
+// convert the argument to integer
 const intValue = parseInt(arg);
 
- //let check if the result is not NaN
+// let check if the result is not NaN
 if (!isNaN(intValue)) {
- //print integer
-console.log(`My number: ${intValue}`);
+  // print integer
+  console.log(`My number: ${intValue}`);
 } else {
- //print Not a number
-console.log('Not a number');
+  // print Not a number
+  console.log('Not a number');
 }
