d68eb50adbea9aff553d3bf690a326cc587e23e9
“mavine4512” <mavineoyugi@gmail.com>
2023-11-20 14:29:28 -0800

    fixed error

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -16,10 +16,10 @@ if __name__ == "__main__":
     from the db.
     """
 
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            argv[1], argv[2], argv[3])
+    db_uri = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
 
-    engine = create_engine(db_url)
+    engine = create_engine(db_uri)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
 
