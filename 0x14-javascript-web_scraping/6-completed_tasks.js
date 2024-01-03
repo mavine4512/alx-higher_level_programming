186fe6c9bc216ca13b97d7de367cb8b0baedcd42
“mavine4512” <mavineoyugi@gmail.com>
2024-01-03 08:09:47 -0800

    fixed errors

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,21 +3,20 @@
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url, {json: true}, (error, response, body) => {
-	if (error) {
-		console.log(error);
-		return;
-	}
-
-	const tasksCompleted = {};
-	body.forEach((todo) => {
-		if (todo.completed) {
-			if (!taskCompleted[todo.userId]){
-				taskCompleted[todo.userId] = 1;
-			} else {
-				taskCompleted[todo.userId] += 1;
-			}
-		}
-	});
-	console.log(tasksCompleted);
+request.get(url, { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const tasksCompleted = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId]) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(tasksCompleted);
 });
