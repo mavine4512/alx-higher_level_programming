5b34e3073339c2e78da91d3433f9077cfe4a9278
“mavine4512” <mavineoyugi@gmail.com>
2024-01-03 04:47:31 -0800

    fixed task

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = request('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request.get(url, (error, response, body) => {
+        if (error) {
+                console.log(error);
+        } else {
+               fs.writeFile(file, body, 'utf8', (error) => {
+		       if (error) {
+			       console.log(error);
+		       }
+	       });
+        }
+});
